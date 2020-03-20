fd8b0cdec49ec00acb79af0522d94948dcd58ccd
bcrazydreamer <bcrazydreamer@gmail.com>
2020-03-20 17:28:54 +0530

    ^

--- config/server_overwrite.config.js
+++ config/server_overwrite.config.js
@@ -1,3 +1,7 @@
+/**
+ * This is hidden or in .gitignore for production
+ * Now its for demo only
+ */
 module.exports = {
     recaptcha_site_key : "6LfjpOIUAAAAAN1xM3o6CDdQVNg9ONBxZUdMbx7l",
     recaptcha_secret_key : "6LfjpOIUAAAAAD-1w7V2-x72OXQSo0JW3H90XH-h"
